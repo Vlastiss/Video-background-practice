6e5470062b6a43e76d7f296538225730e886601f
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-15 20:41:40 +0000

    update

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
+
     $(".form-check-input").click(function () { 
-        $(".form-check-input").addAtr(checked);
+
+        var videoContainer = $(".video-container")[0];
+
+        if ($(this).prop("checked")) {
+            $(".text-info").html("Playing")
+            videoContainer.play();
+        } else {
+            $(".text-info").html("Paused")
+            videoContainer.pause();
+        }
     });
+
 });
